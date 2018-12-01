d0c8149cd5ed5ae2e0fbcca5f962e4c5612cdfc2
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-12-01 11:26:54 -0800

    [WIP] Successfully returning listings upon get call

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path')
 const app = express();
+const db = require('./database/db')
 
 // Handling errors to be communicated to client
 const errors = {}
@@ -14,6 +15,8 @@ app.use(bodyParser.json());
 // Serving Static Files
 app.use(express.static(path.join(__dirname, '../client/public')));
 
+// 
+
 
 // Load the BookedDates Model
 const BookedDates = require('./models/BookedDates');
@@ -23,13 +26,17 @@ const BookedDates = require('./models/BookedDates');
 // @desc      Gets all booked dates for a listing
 // @access    Public
 app.get('/api/dates', (req, res) => {
-  console.log(req.params.id)
-  BookedDates.findOne({listing_id: 1})
-    .then(dates => {
-      console.log(dates)
+  console.log('receiving request')
+  BookedDates.findOne({'listing_id': 1})
+    .then(listing => {
+      
+      console.log('Listing', listing)
+      res.json(listing)
     })
-    .catch(err => res.status(404).json({listingnotfound: 'No listing found' }))
-})
+    .catch(err => {
+      console.log(err)
+     })
+ })
 
 
 // Declaring Server Port
