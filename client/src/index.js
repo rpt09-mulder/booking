2d9f98cb506f2dc74049005db07b794236bd96c3
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-12-15 12:28:57 -0800

    [WIP] Switching out react-dates with reactdatepicker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
 
+window.Booking = App;
+
+
+console.log('leslie', document)
+
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
+  document.getElementById('booking')
 );
+
+
