a679e901336b7e0827b0fcf87d6d1b1a676628b4
Philipp A <33808429+insivika@users.noreply.github.com>
2018-12-22 13:05:05 -0800

    Configured servier to Handle date range (#8)
    * [WIP] Adjusted submit handler to send start and end date
    
    * [WIP] Successfully storing dates to database and sending them back to the client to be reflected as bookd.
    
    * [WIP] Adjusted server to handle date ranges

--- client/src/components/Booker.js
+++ client/src/components/Booker.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import DatePicker from "react-datepicker";
-import { isAfter } from 'date-fns';
 import axios from 'axios';
-import Moment from 'react-moment';
 
 const moment = require('moment')
 
 import "react-datepicker/dist/react-datepicker.css";
-
 
 class Booker extends React.Component {
    state={
@@ -23,8 +20,8 @@ class Booker extends React.Component {
     if (window.location.pathname !== '/') {
       id = window.location.pathname;
     }
-
-    axios.get(`http://booking-dev2.us-west-1.elasticbeanstalk.com/booking/dates${id}`)
+    //http://booking-dev2.us-west-1.elasticbeanstalk.com
+    axios.get(`http://localhost:3004/booking/dates${id}`)
     .then(result => {
       this.setState({
         bookedDates: result.data.bookedDates
@@ -38,14 +35,21 @@ class Booker extends React.Component {
 
 
   handleSubmitBooking = () =>{
-     axios.post(`http://booking-dev2.us-west-1.elasticbeanstalk.com/booking/dates${window.location.pathname}`, {
-        newBookedDate: this.state.startDate
+    
+    const startDate = moment(this.state.startDate).startOf('day')
+    const endDate = moment(this.state.endDate).startOf('day')
+
+     axios.post(`http://localhost:3004/booking/dates${window.location.pathname}`, {
+        startDate: startDate,
+        endDate: endDate
       })
       .then(result => {
 
         this.handleGetBookedDates()
         
         this.setState({
+          startDate: new Date(),
+          endDate: new Date(),
           message: result.data
         })
 
@@ -73,8 +77,6 @@ class Booker extends React.Component {
   // Triggerts handleChange for start date
   handleChangeStart = (startDate) => {
 
-    console.log('startDate', startDate)
-
     startDate = startDate || this.state.startDate;
 
     let endDate = this.state.endDate;
@@ -91,14 +93,12 @@ class Booker extends React.Component {
   // Triggers handleChage for end date
   handleChangeEnd = (endDate) => {
 
-    console.log('endDate', endDate)
 
     endDate = endDate || this.state.endDate;
 
     const startDate = this.state.startDate
 
     if(moment(startDate).isAfter(endDate)){
-      console.log('true')
       endDate = startDate
     }
 
@@ -111,8 +111,17 @@ class Booker extends React.Component {
   }
 
   render(){
-    return(
 
+    const modal = (
+
+      <div className="booking-modal">
+        <h3>Your Dates Have Been Booked!</h3>
+      </div>
+
+    )
+
+    return(
+    
       <div>
       <DatePicker
           selected={this.state.startDate}
@@ -136,6 +145,13 @@ class Booker extends React.Component {
           onChange={this.handleChangeEnd}
       />
 
+      <div>
+        <button onClick={this.handleSubmitBooking}>Book Dates</button>
+      </div>
+
+      {this.state.message !== '' ? (
+        modal
+      ) : ''}
       </div>
     )
   }
